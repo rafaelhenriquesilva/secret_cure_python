9c6ffb6a04dbddea4767f07b28c6c59c5f357183
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-20 01:48:14 -0300

    correcting order tests json

--- __tests__/test_json.py
+++ __tests__/test_json.py
@@ -8,6 +8,17 @@ from src.utils.json_util import JSONUtil
 from src.useCases.user_use_case import UserUseCase
 from src.models.user_model import UserInfo
 
+def test_change_user_info_and_json():
+    fake_data_json = JSONUtil.loadJson('fake.json')
+    user_info = UserInfo('Test', 1, 'test@mail.com')
+    fake_data_json = UserUseCase.updateUserInfo(user_info, fake_data_json)
+    JSONUtil.createJson(fake_data_json, 'fake.json')
+
+    update_data_json = JSONUtil.loadJson('fake.json')
+    assert update_data_json['user_info']['name'] == user_info.name
+    assert update_data_json['user_info']['email'] == user_info.email
+    assert update_data_json['user_info']['level'] == user_info.level
+
 def test_read_user_infos_json():
     data_json = JSONUtil.loadJson('fake.json')
     assert len(data_json['levels_allowed']) > 0
@@ -22,15 +33,6 @@ def test_create_json():
     assert len(fake_data_json['user_info']['name']) != ''
     assert len(fake_data_json['user_info']['email']) != ''    
 
-def test_change_user_info_and_json():
-    fake_data_json = JSONUtil.loadJson('fake.json')
-    user_info = UserInfo('Test', 'test@mail.com', 1)
-    fake_data_json = UserUseCase.updateUserInfo(user_info, fake_data_json)
-    JSONUtil.createJson(fake_data_json, 'fake.json')
 
-    update_data_json = JSONUtil.loadJson('fake.json')
-    assert update_data_json['user_info']['name'] == user_info.name
-    assert update_data_json['user_info']['email'] == user_info.email
-    assert update_data_json['user_info']['level'] == user_info.level
 
     
